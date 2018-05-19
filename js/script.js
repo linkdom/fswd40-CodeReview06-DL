26276cf085e6ad40df4afa405f070923ce86401c
Dominik Link <Vorbild94@gmail.com>
2018-05-19 16:53:29 +0200

    Final version

---
+++ js/script.js
@@ -0,0 +1,120 @@
+var books = [{
+        "title": "Odyssee",
+        "author": "Homer",
+        "publisher": "Gareth Hinds",
+        "image": "img/homer.jpg",
+        "description": "Mancher Literaturliebhaber behauptet, die Schrift sei nur deswegen erfunden worden, um die epischen Erzählungen Homers niederzuschreiben. Das ist vielleicht ein wenig zu viel der Schmeichelei, aber Fakt ist: Homers Odyssee gehört zu den ältesten, wichtigsten und größten Erzählungen der Weltliteratur. Ein angeblich blinder Dichter, von dem wir heute fast nichts wissen, schenkte uns diese gewaltige Abenteuererzählung: Nach zehn Jahren im Trojanischen Krieg will der Held Odysseus endlich in die geliebte Heimat zurückkehren. Doch er erzürnt den Meergott Poseidon, was ihm manches Ungemach beschert. Mann für Mann verliert er seine Gefährten, er begegnet den menschenfressenden Laistrygonen, dem einäugigen Kyklopen, dem sechsköpfigen Seeungeheuer Skylla, den verlockenden Sirenen, der Zauberin Kirke und landet schließlich auf einem Eiland bei der Nymphe Kalypso. Nur durch den Ratschluss der Götter darf er nach zehnjähriger Irrfahrt in die Heimat zurückkehren, Frau und Thron wieder in Besitz nehmen und Ordnung schaffen im verwahrlosten Königreich. Der französische Schriftsteller George Duhamel empfahl einmal, dass \"ein kultivierter Europäer alle zehn Jahre die Homer'schen Epen wiederlesen\" soll. Es lohnt sich, denn mit Homer beginnt tatsächlich die europäische Literatur.",
+        "rating": "&starf;&starf;&starf;&starf;&starf;"
+    }, {
+        "title": "Das Bildnis des Dorian Gray",
+        "author": "Oscar Wilde",
+        "publisher": "Lock and Co.",
+        "image": "img/gray.jpg",
+        "description": "Die Handlung setzt mit einem Gespräch zweier junger Männer ein, Lord Henry Wotton (genannt Lord Henry oder Harry), eines gebildeten Dandys, und des erfolgreichen Malers Basil Hallward. Ort des Geschehens ist Basils Atelier, das sich in einen Garten öffnet. Es geht um Kunst und Selbstinszenierung.",
+        "rating": "&starf;&starf;&starf;&starf;&starf;"
+    }, {
+        "title": "Stolz und Vorurteil",
+        "author": "Jane Austen",
+        "publisher": "Thomas Egerton",
+        "image": "img/suv.jpg",
+        "description": "Stolz und Vorurteil ist ein Entwicklungsroman, in dem sich die beiden Hauptfiguren Elizabeth Bennet und Fitzwilliam Darcy durch die Überwindung einiger Krisen verändern, um sich in neuer Bescheidenheit und Einsicht in ihre Fehler für eine gemeinsame Zukunft zu finden.",
+        "rating": "&starf;&starf;&starf;&starf;&starf;"
+    }];
+var cd = [{
+        "title": "Hold on Tight",
+        "author": "R3HAB and Conor Maynard",
+        "publisher": "CYB3RPVNK",
+        "image": "img/hot.jpg",
+        "genre": "Dance/Electro",
+        "rating": "&starf;&starf;&starf;&starf;&starf;"
+    }, {
+        "title": "Mad Love",
+        "author": "Sean Paul, David Guetta",
+        "publisher": "SPJ Productions Ltd, under exclusive licence to Island Records, a division of Universal Music Operations Limited",
+        "image": "img/mad-love.jpg",
+        "genre": "Dance/Electro/Pop",
+        "rating": "&starf;&starf;&starf;&starf;&starf;"
+    }, {
+        "title": "One Kiss",
+        "author": "Calvin Harris, Dua Lipa",
+        "publisher": "SME (im Auftrag von Columbia); UMPG Publishing, ASCAP, Heaven 11, EMI Music Publishing",
+        "image": "img/kiss.jpg",
+        "genre": "Dance/Electro",
+        "rating": "&starf;&starf;&starf;&starf;&starf;"
+    }];
+var dvd = [{
+        "title": "Das Streben nach Glück",
+        "author": "	Gabriele Muccino",
+        "image": "img/poh.jpg",
+        "description": "Chris Gardner lebt Anfang der 1980er Jahre in San Francisco und arbeitet als selbstständiger Handelsvertreter. Er versucht, neuartige Knochendichtemessgeräte zu verkaufen, die eine geringfügig bessere Auflösung als bisher übliche Geräte ermöglichen. Da diese jedoch doppelt so teuer sind, ist Gardner nicht sonderlich erfolgreich. Er wird von seiner Frau Linda verlassen, die die finanziellen Engpässe der Familie nicht mehr aushalten kann. Nachdem Linda mit dem gemeinsamen Sohn Christopher weggegangen ist, holt Chris seinen Sohn zu sich zurück, da er weiß, dass er wegen seines unbeugsamen Ehrgeizes besser für ihn sorgen kann als Linda, die die Belastung nicht mehr verkraftet, was Linda letzten Endes auch einsieht.",
+        "rating": "&starf;&starf;&starf;&starf;&starf;"
+    }, {
+        "title": "Deadpool 2",
+        "author": "David Leitch",
+        "image": "img/deadpool2.jpg",
+        "description": "Zwei Jahre nach den Ereignissen des ersten Films ist Deadpool / Wade Wilson ein Söldner geworden, der weltweit arbeitet und verschiedene Kriminelle tötet. Nach einem abgebrochenen Anschlag auf den Anführer eines Drogenkartells in seiner Heimatstadt kehrt Deadpool nach Hause zu seiner Freundin Vanessa Carlysle zurück, um ihren Jahrestag zu feiern. Deadpool schenkt Vanessa ein Skee-Ball-Token, in Bezug auf ihre erste Verabredung, und die beiden stimmen zu, eine Familie zu gründen. Später in der Nacht greift der Drogenboss Wade bei ihm zu Hause an, wobei Vanessa stirbt. Wade jagt den Verbrecher und umarmt ihn schließlich auf einer Straße, als ein Lastwagen in die beiden hineinfährt und den Drogenboss tötet.",
+        "rating": "&starf;&starf;&starf;&starf;&starf;"
+    }, {
+        "title": "Interstellar",
+        "author": "Christopher Nolan",
+        "image": "img/interstellar.jpg",
+        "description": "In der zweiten Hälfte des 21. Jahrhunderts ist die irdische Biosphäre so stark geschädigt, dass die Zukunft der gesamten Menschheit unmittelbar bedroht ist. Die industrielle Zivilisation befindet sich im Niedergang. Die Menschheit wurde stark dezimiert und die Überlebenden konzentrieren sich vorwiegend auf die Produktion von Nahrungsmitteln, deren Anbau jedoch durch sich immer weiter ausbreitende Pflanzenkrankheiten, erosionsbedingte Staubstürme und sich generell verschlechternde Umweltbedingungen zunehmend erschwert wird.",
+        "rating": "&starf;&starf;&starf;&starf;&starf;"
+    }];
+var comic = [{
+        "title": "Batman: Lächeln, bitte!",
+        "publisher": "DC Comics",
+        "image": "img/batman.jpg",
+        "rating": "&starf;&starf;&starf;&starf;&starf;"
+    }, {
+        "title": "Venom",
+        "publisher": "Marvel",
+        "image": "img/venom.jpg",
+        "rating": "&starf;&starf;&starf;&starf;&starf;"
+    }, {
+        "title": "X-Men Prime",
+        "publisher": "Marvel",
+        "image": "img/x-men.jpg",
+        "rating": "&starf;&starf;&starf;&starf;&starf;"
+    }];
+for (var _i = 0, books_1 = books; _i < books_1.length; _i++) {
+    var indexBooks = books_1[_i];
+    document.getElementById("books").innerHTML += "\n\t\t<div class=\"col-lg-4 col-md-4 col-sm-12 col-xs-12 panel\">\n\t\t\t<img src=\"" + indexBooks.image + "\" alt=\"Homer-Odysee\" class=\"img-responsive center-block\">\n\t\t\t<div class=\"caption align\">\n\t\t\t\t<h3>" + indexBooks.title + "</h3>\n\t\t\t\t<span>von <a href=\"#\">" + indexBooks.author + "</a></span>\n\t\t\t\t<br>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"small\">(853)</span>\n\t\t\t</div>\n\t\t</div>\n\t";
+}
+for (var _a = 0, dvd_1 = dvd; _a < dvd_1.length; _a++) {
+    var indexDvd = dvd_1[_a];
+    document.getElementById("dvds").innerHTML += "\n\t\t<div class=\"col-lg-4 col-md-4 col-sm-12 col-xs-12 panel\">\n\t\t\t<img src=\"" + indexDvd.image + "\" alt=\"Homer-Odysee\" class=\"img-responsive center-block\">\n\t\t\t<div class=\"caption align\">\n\t\t\t\t<h3>" + indexDvd.title + "</h3>\n\t\t\t\t<span>von <a href=\"#\">" + indexDvd.author + "</a></span>\n\t\t\t\t<br>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"small\">(330)</span>\n\t\t\t</div>\n\t\t</div>\n\t";
+}
+for (var _b = 0, comic_1 = comic; _b < comic_1.length; _b++) {
+    var indexComic = comic_1[_b];
+    document.getElementById("comics").innerHTML += "\n\t\t<div class=\"col-lg-4 col-md-4 col-sm-12 col-xs-12 panel\">\n\t\t\t<img src=\"" + indexComic.image + "\" alt=\"Homer-Odysee\" class=\"img-responsive center-block\">\n\t\t\t<div class=\"caption align\">\n\t\t\t\t<h3>" + indexComic.title + "</h3>\n\t\t\t\t<span>von <a href=\"#\">" + indexComic.publisher + "</a></span>\n\t\t\t\t<br>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"small\">(357)</span>\n\t\t\t</div>\n\t\t</div>\n\t";
+}
+for (var _c = 0, cd_1 = cd; _c < cd_1.length; _c++) {
+    var indexCd = cd_1[_c];
+    document.getElementById("cds").innerHTML += "\n\t\t<div class=\"col-lg-4 col-md-4 col-sm-12 col-xs-12 panel\">\n\t\t\t<img src=\"" + indexCd.image + "\" alt=\"Homer-Odysee\" class=\"img-responsive center-block\">\n\t\t\t<div class=\"caption align\">\n\t\t\t\t<h3>" + indexCd.title + "</h3>\n\t\t\t\t<span>von <a href=\"#\">" + indexCd.author + "</a></span>\n\t\t\t\t<br>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"glyphicon glyphicon-star\"></span>\n\t\t\t\t<span class=\"small\">(260)</span>\n\t\t\t</div>\n\t\t</div>\n\t";
+}
+submit.onclick = function getData() {
+    var title = document.getElementById("title").value;
+    var author = document.getElementById("author").value;
+    var publisher = document.getElementById("publisher").value;
+    var img = document.getElementById("img").value;
+    var description = document.getElementById("descriptions").value;
+    var a = document.getElementById("selectType");
+    var tempType = a.options[a.selectedIndex].text;
+    var b = document.getElementById("selectGenreMusic");
+    var tempGenreMusic = b.options[b.selectedIndex].text;
+    var c = document.getElementById("selectGenreGeneral");
+    var tempGenreGeneral = c.options[c.selectedIndex].text;
+    if (tempType == "Book") {
+        document.getElementById("books").innerHTML += "\n <div class=\"col-lg-4 col-md-4 col-sm-12 col-xs-12 panel\">\n  <img src=\"" + img + "\" alt=\"" + title + "\" class=\"img-responsive center-block\">\n  <div class=\"caption align\">\n   <h3>" + title + "</h3>\n   <span>von <a href=\"#\">" + author + "</a></span>\n   <br>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"small\">(853)</span>\n  </div>\n </div>\n";
+    }
+    else if (tempType == "DVD") {
+        document.getElementById("dvds").innerHTML += "\n <div class=\"col-lg-4 col-md-4 col-sm-12 col-xs-12 panel\">\n  <img src=\"" + img + "\" alt=\"" + title + "\" class=\"img-responsive center-block\">\n  <div class=\"caption align\">\n   <h3>" + title + "</h3>\n   <span>von <a href=\"#\">" + author + "</a></span>\n   <br>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"small\">(330)</span>\n  </div>\n </div>\n";
+    }
+    else if (tempType == "Comic") {
+        document.getElementById("comics").innerHTML += "\n <div class=\"col-lg-4 col-md-4 col-sm-12 col-xs-12 panel\">\n  <img src=\"" + img + "\" alt=\"" + title + "\" class=\"img-responsive center-block\">\n  <div class=\"caption align\">\n   <h3>" + title + "</h3>\n   <span>von <a href=\"#\">" + author + "</a></span>\n   <br>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"small\">(357)</span>\n  </div>\n </div>\n";
+    }
+    else {
+        document.getElementById("cds").innerHTML += "\n <div class=\"col-lg-4 col-md-4 col-sm-12 col-xs-12 panel\">\n  <img src=\"" + img + "\" alt=\"" + title + "\" class=\"img-responsive center-block\">\n  <div class=\"caption align\">\n   <h3>" + title + "</h3>\n   <span>von <a href=\"#\">" + author + "</a></span>\n   <br>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"glyphicon glyphicon-star\"></span>\n   <span class=\"small\">(260)</span>\n  </div>\n </div>\n";
+    }
+};
---
+++ js/script.ts
@@ -0,0 +1,244 @@
+
+const books = [{
+	"title":"Odyssee",
+	"author":"Homer",
+	"publisher":"Gareth Hinds",
+	"image":"img/homer.jpg",
+	"description":"Mancher Literaturliebhaber behauptet, die Schrift sei nur deswegen erfunden worden, um die epischen Erzählungen Homers niederzuschreiben. Das ist vielleicht ein wenig zu viel der Schmeichelei, aber Fakt ist: Homers Odyssee gehört zu den ältesten, wichtigsten und größten Erzählungen der Weltliteratur. Ein angeblich blinder Dichter, von dem wir heute fast nichts wissen, schenkte uns diese gewaltige Abenteuererzählung: Nach zehn Jahren im Trojanischen Krieg will der Held Odysseus endlich in die geliebte Heimat zurückkehren. Doch er erzürnt den Meergott Poseidon, was ihm manches Ungemach beschert. Mann für Mann verliert er seine Gefährten, er begegnet den menschenfressenden Laistrygonen, dem einäugigen Kyklopen, dem sechsköpfigen Seeungeheuer Skylla, den verlockenden Sirenen, der Zauberin Kirke und landet schließlich auf einem Eiland bei der Nymphe Kalypso. Nur durch den Ratschluss der Götter darf er nach zehnjähriger Irrfahrt in die Heimat zurückkehren, Frau und Thron wieder in Besitz nehmen und Ordnung schaffen im verwahrlosten Königreich. Der französische Schriftsteller George Duhamel empfahl einmal, dass \"ein kultivierter Europäer alle zehn Jahre die Homer'schen Epen wiederlesen\" soll. Es lohnt sich, denn mit Homer beginnt tatsächlich die europäische Literatur.",
+	"rating":"&starf;&starf;&starf;&starf;&starf;"
+},{
+	"title":"Das Bildnis des Dorian Gray",
+	"author":"Oscar Wilde",
+	"publisher":"Lock and Co.",
+	"image":"img/gray.jpg",
+	"description":"Die Handlung setzt mit einem Gespräch zweier junger Männer ein, Lord Henry Wotton (genannt Lord Henry oder Harry), eines gebildeten Dandys, und des erfolgreichen Malers Basil Hallward. Ort des Geschehens ist Basils Atelier, das sich in einen Garten öffnet. Es geht um Kunst und Selbstinszenierung.",
+	"rating":"&starf;&starf;&starf;&starf;&starf;"
+},{
+	"title":"Stolz und Vorurteil",
+	"author":"Jane Austen",
+	"publisher":"Thomas Egerton",
+	"image":"img/suv.jpg",
+	"description":"Stolz und Vorurteil ist ein Entwicklungsroman, in dem sich die beiden Hauptfiguren Elizabeth Bennet und Fitzwilliam Darcy durch die Überwindung einiger Krisen verändern, um sich in neuer Bescheidenheit und Einsicht in ihre Fehler für eine gemeinsame Zukunft zu finden.",
+	"rating":"&starf;&starf;&starf;&starf;&starf;"
+}];
+
+const cd = [{
+	"title":"Hold on Tight",
+	"author":"R3HAB and Conor Maynard",
+	"publisher":"CYB3RPVNK",
+	"image":"img/hot.jpg",
+	"genre":"Dance/Electro",
+	"rating":"&starf;&starf;&starf;&starf;&starf;"
+},{
+	"title":"Mad Love",
+	"author":"Sean Paul, David Guetta",
+	"publisher":"SPJ Productions Ltd, under exclusive licence to Island Records, a division of Universal Music Operations Limited",
+	"image":"img/mad-love.jpg",
+	"genre":"Dance/Electro/Pop",
+	"rating":"&starf;&starf;&starf;&starf;&starf;"
+},{
+	"title":"One Kiss",
+	"author":"Calvin Harris, Dua Lipa",
+	"publisher":"SME (im Auftrag von Columbia); UMPG Publishing, ASCAP, Heaven 11, EMI Music Publishing",
+	"image":"img/kiss.jpg",
+	"genre":"Dance/Electro",
+	"rating":"&starf;&starf;&starf;&starf;&starf;"
+}];
+
+const dvd = [{
+	"title":"Das Streben nach Glück",
+	"author":"	Gabriele Muccino",
+	"image":"img/poh.jpg",
+	"description":"Chris Gardner lebt Anfang der 1980er Jahre in San Francisco und arbeitet als selbstständiger Handelsvertreter. Er versucht, neuartige Knochendichtemessgeräte zu verkaufen, die eine geringfügig bessere Auflösung als bisher übliche Geräte ermöglichen. Da diese jedoch doppelt so teuer sind, ist Gardner nicht sonderlich erfolgreich. Er wird von seiner Frau Linda verlassen, die die finanziellen Engpässe der Familie nicht mehr aushalten kann. Nachdem Linda mit dem gemeinsamen Sohn Christopher weggegangen ist, holt Chris seinen Sohn zu sich zurück, da er weiß, dass er wegen seines unbeugsamen Ehrgeizes besser für ihn sorgen kann als Linda, die die Belastung nicht mehr verkraftet, was Linda letzten Endes auch einsieht."
+	"rating":"&starf;&starf;&starf;&starf;&starf;"
+},{
+	"title":"Deadpool 2",
+	"author":"David Leitch",
+	"image":"img/deadpool2.jpg",
+	"description":"Zwei Jahre nach den Ereignissen des ersten Films ist Deadpool / Wade Wilson ein Söldner geworden, der weltweit arbeitet und verschiedene Kriminelle tötet. Nach einem abgebrochenen Anschlag auf den Anführer eines Drogenkartells in seiner Heimatstadt kehrt Deadpool nach Hause zu seiner Freundin Vanessa Carlysle zurück, um ihren Jahrestag zu feiern. Deadpool schenkt Vanessa ein Skee-Ball-Token, in Bezug auf ihre erste Verabredung, und die beiden stimmen zu, eine Familie zu gründen. Später in der Nacht greift der Drogenboss Wade bei ihm zu Hause an, wobei Vanessa stirbt. Wade jagt den Verbrecher und umarmt ihn schließlich auf einer Straße, als ein Lastwagen in die beiden hineinfährt und den Drogenboss tötet."
+	"rating":"&starf;&starf;&starf;&starf;&starf;"
+},{
+	"title":"Interstellar",
+	"author":"Christopher Nolan",
+	"image":"img/interstellar.jpg",
+	"description":"In der zweiten Hälfte des 21. Jahrhunderts ist die irdische Biosphäre so stark geschädigt, dass die Zukunft der gesamten Menschheit unmittelbar bedroht ist. Die industrielle Zivilisation befindet sich im Niedergang. Die Menschheit wurde stark dezimiert und die Überlebenden konzentrieren sich vorwiegend auf die Produktion von Nahrungsmitteln, deren Anbau jedoch durch sich immer weiter ausbreitende Pflanzenkrankheiten, erosionsbedingte Staubstürme und sich generell verschlechternde Umweltbedingungen zunehmend erschwert wird.",
+	"rating":"&starf;&starf;&starf;&starf;&starf;"
+}];
+
+const comic = [{
+	"title":"Batman: Lächeln, bitte!",
+	"publisher":"DC Comics",
+	"image":"img/batman.jpg",
+	"rating":"&starf;&starf;&starf;&starf;&starf;"
+},{
+	"title":"Venom",
+	"publisher":"Marvel",
+	"image":"img/venom.jpg",
+	"rating":"&starf;&starf;&starf;&starf;&starf;"
+},{
+	"title":"X-Men Prime",
+	"publisher":"Marvel",
+	"image":"img/x-men.jpg",
+	"rating":"&starf;&starf;&starf;&starf;&starf;"
+}];
+
+for(let indexBooks of books) {
+	document.getElementById("books").innerHTML += `
+		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 panel">
+			<img src="${indexBooks.image}" alt="Homer-Odysee" class="img-responsive center-block">
+			<div class="caption align">
+				<h3>${indexBooks.title}</h3>
+				<span>von <a href="#">${indexBooks.author}</a></span>
+				<br>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="small">(853)</span>
+			</div>
+		</div>
+	`
+}
+
+for(let indexDvd of dvd) {
+	document.getElementById("dvds").innerHTML += `
+		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 panel">
+			<img src="${indexDvd.image}" alt="Homer-Odysee" class="img-responsive center-block">
+			<div class="caption align">
+				<h3>${indexDvd.title}</h3>
+				<span>von <a href="#">${indexDvd.author}</a></span>
+				<br>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="small">(330)</span>
+			</div>
+		</div>
+	`
+}
+
+for(let indexComic of comic) {
+	document.getElementById("comics").innerHTML += `
+		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 panel">
+			<img src="${indexComic.image}" alt="Homer-Odysee" class="img-responsive center-block">
+			<div class="caption align">
+				<h3>${indexComic.title}</h3>
+				<span>von <a href="#">${indexComic.publisher}</a></span>
+				<br>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="small">(357)</span>
+			</div>
+		</div>
+	`
+}
+
+for(let indexCd of cd) {
+	document.getElementById("cds").innerHTML += `
+		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 panel">
+			<img src="${indexCd.image}" alt="Homer-Odysee" class="img-responsive center-block">
+			<div class="caption align">
+				<h3>${indexCd.title}</h3>
+				<span>von <a href="#">${indexCd.author}</a></span>
+				<br>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="glyphicon glyphicon-star"></span>
+				<span class="small">(260)</span>
+			</div>
+		</div>
+	`
+}
+
+submit.onclick = function getData() {
+var title = document.getElementById("title").value;
+var author = document.getElementById("author").value;
+var publisher = document.getElementById("publisher").value;
+var img = document.getElementById("img").value;
+var description = document.getElementById("descriptions").value;
+var a = document.getElementById("selectType");
+var tempType = a.options[a.selectedIndex].text;
+var b = document.getElementById("selectGenreMusic");
+var tempGenreMusic = b.options[b.selectedIndex].text;
+var c = document.getElementById("selectGenreGeneral");
+var tempGenreGeneral = c.options[c.selectedIndex].text;
+
+if(tempType == "Book"){
+ document.getElementById("books").innerHTML += `
+ <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 panel">
+  <img src="${img}" alt="${title}" class="img-responsive center-block">
+  <div class="caption align">
+   <h3>${title}</h3>
+   <span>von <a href="#">${author}</a></span>
+   <br>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="small">(853)</span>
+  </div>
+ </div>
+`
+} else if(tempType == "DVD"){
+ document.getElementById("dvds").innerHTML += `
+ <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 panel">
+  <img src="${img}" alt="${title}" class="img-responsive center-block">
+  <div class="caption align">
+   <h3>${title}</h3>
+   <span>von <a href="#">${author}</a></span>
+   <br>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="small">(330)</span>
+  </div>
+ </div>
+`
+} else if(tempType == "Comic"){
+ document.getElementById("comics").innerHTML += `
+ <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 panel">
+  <img src="${img}" alt="${title}" class="img-responsive center-block">
+  <div class="caption align">
+   <h3>${title}</h3>
+   <span>von <a href="#">${author}</a></span>
+   <br>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="small">(357)</span>
+  </div>
+ </div>
+`
+} else{
+ document.getElementById("cds").innerHTML += `
+ <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 panel">
+  <img src="${img}" alt="${title}" class="img-responsive center-block">
+  <div class="caption align">
+   <h3>${title}</h3>
+   <span>von <a href="#">${author}</a></span>
+   <br>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="glyphicon glyphicon-star"></span>
+   <span class="small">(260)</span>
+  </div>
+ </div>
+`
+}
+
+}
